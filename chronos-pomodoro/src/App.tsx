9be454a0d8d50c7d317c8d7b226d1205b895f5d6
NicholasDRR <nicholasreis00@gmail.com>
2025-08-11 21:01:30 -0300

    08-11-25

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -4,8 +4,14 @@ import "./styles/themes.css";
 import "./styles/global.css";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
 
 export function App() {
+
   return (
     <>
       <Container>
@@ -15,9 +21,37 @@ export function App() {
         <Menu />
       </Container>
       <Container>
-        <CountDown/>
+        <CountDown />
       </Container>
-
+      <Container>
+        <form action="" className="form">
+          <div className="formRow ">
+            <DefaultInput
+              id="meuInput"
+              type="text"
+              labelText="Texto"
+              placeholder="Digite algo"
+              defaultValue={"Valor preenchido"}
+              disabled
+            />
+          </div>
+          <div className="formRow ">
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className="formRow ">
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className="formRow ">
+            <Cycles />
+          </div>
+          <div className="formRow ">
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+          </div>
+          <div className="formRow ">
+            <Footer />
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,19 +1,56 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    setTheme((prevTheme) => {
+      const nexTheme = prevTheme === "dark" ? "light" : "dark";
+      return nexTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        aria-label="Ir para home"
+        title="Ir para home"
+        className={styles.menuLink}
+      >
         <HouseIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        aria-label="Ir para histórico"
+        title="Ir para histórico"
+        className={styles.menuLink}
+      >
         <HistoryIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        aria-label="Ir para configurações"
+        title="Ir para configurações"
+        className={styles.menuLink}
+      >
         <SettingsIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        aria-label="Mudar tema visual"
+        title="Mudar tema visual"
+        className={styles.menuLink}
+        onClick={handleTheme}
+      >
         <SunIcon />
       </a>
     </nav>
